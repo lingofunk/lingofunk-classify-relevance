d0edf3950f9e617d5bf20fc441d3aff7bdc2b0ef
s231644 <daniil.vodolazsky@mail.ru>
2019-02-07 14:05:17 +0300

    Replaced dataset creating with generator

--- train_classifier.py
+++ train_classifier.py
@@ -101,7 +101,7 @@ def train():
 
     try:
         preprocesor = pickle.load(PRERPOCESSOR_FILE)
-        yelp_dataset_generator.preprocess(preprocesor, True)
+        yelp_dataset_generator.preprocess(preprocesor)
 
         logger.info("Opened preprocessing file.")
     except:
