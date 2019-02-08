21d5061d382c77e7c757b3641ab6dc852621f320
s231644 <daniil.vodolazsky@mail.ru>
2019-02-08 16:13:12 +0300

    Changed batch_size.

--- train_classifier_attn.py
+++ train_classifier_attn.py
@@ -88,7 +88,7 @@ def get_model(maxlen, max_features, lstm_size, rate_drop_lstm, rate_drop_dense, 
     embedding_layer_1 = emb_layer(input_1)
     embedding_layer_2 = emb_layer(input_2)
     # embedded_sequences = concatenate([embedding_layer_1, embedding_layer_2], axis=-1)
-    embedded_sequences = Add()([embedding_layer_1, embedding_layer_2], axis=-1)
+    embedded_sequences = Add()([embedding_layer_1, embedding_layer_2])
     x = Bidirectional(LSTM(lstm_size,
                            dropout=rate_drop_lstm,
                            recurrent_dropout=rate_drop_lstm,
