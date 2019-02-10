4e032cdc4907887b307b5e1270e06de9f0b1c9a6
s231644 <daniil.vodolazsky@mail.ru>
2019-02-10 15:13:55 +0300

    Added Mult

--- mult.py
+++ mult.py
@@ -60,7 +60,7 @@ def get_model(maxlen, max_features, lstm_size, rate_drop_lstm, rate_drop_dense, 
 
 def train():
     config = tf.ConfigProto()
-    config.gpu_options.per_process_gpu_memory_fraction = 0.25
+    config.gpu_options.per_process_gpu_memory_fraction = 0.2
     set_session(tf.Session(config=config))
 
     logger = get_logger()
